be4f9a060d7243d2bc0605f993287597cafe920f
Sai Kiran <poreddysaikiran6@gmail.com>
2026-05-17 06:47:05 +0530

    Clean up unnecessary test files and api cache

--- test.py
+++
@@ -1,7 +0,0 @@
-import os
-os.environ["FLAGS_use_mkldnn"] = "0"
-os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
-from paddleocr import PaddleOCR
-ocr = PaddleOCR(use_angle_cls=False, lang='en', enable_mkldnn=False, use_gpu=False)
-result = ocr.ocr('c:\\MPS\\PaddleOCR 10-03-26\\handwriting-app\\test_image.png', cls=False)
-print(result)
